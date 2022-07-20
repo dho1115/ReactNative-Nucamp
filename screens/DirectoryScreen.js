cb9dd2ef04d9730af62d7bed1eda84b5870ed1b1
DAVID HO <davidhorph@hotmail.com>
2022-07-20 10:45:25 -0500

    MainComponent and DirectoryScreen.

---
+++ screens/DirectoryScreen.js
@@ -0,0 +1,8 @@
+import { FlatList } from "react-native";
+import { Avatar, ListItem } from "react-native-elements";
+
+const DirectoryScreen = (props) => {
+
+}
+
+export default DirectoryScreen;
---
+++ screens/MainComponent.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import { CAMPSITES } from '../shared/campsites';
+import { DirectoryScreen } from './DirectoryScreen';
+
+const Main = () => {
+    const [campsites, setcampsites] = useState(CAMPSITES);
+
+    return <DirectoryScreen campsites={campsites} />
+}
+
+export default Main;
