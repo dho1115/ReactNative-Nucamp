4e8eedf89490e8972e1d246b7dcf90caa0b779ff
DAVID HO <davidhorph@hotmail.com>
2022-07-20 15:55:51 -0500

    Adding campsite.name and campsite.description to RenderCampsite.js.

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -6,7 +6,16 @@ const RenderCampsite = ({ campsite }) => {
     if (campsite) {
         return(
             <Card containerStyle={{ padding: 0 }}>
-                <Card.Image src={{ campsite.image }}></Card.Image>
+                <Card.Image src={{ campsite.image }}>
+                    <View style={{ justifyContent: 'center', flex: 1 }}>
+                        <Text style={{
+                            color: 'white',
+                            textAlign: 'center',
+                            fontSize: 15
+                        }}>{ campsite.name }</Text>
+                    </View>
+                </Card.Image>
+                <Text style={{ margin: 17 }}>{ campsite.description }</Text>
             </Card>
         )
     }
