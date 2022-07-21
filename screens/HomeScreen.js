0a99205d487f53f363108cb321754fa7607f4b8e
DAVID HO <davidhorph@hotmail.com>
2022-07-21 16:36:29 -0500

    Customizing the HomeScreen.js.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,39 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { Text, View, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
+import { PROMOTIONS } from '../shared/promotions';
+import { PARTNERS } from '../shared/partners';
+
+const FeaturedItem = ({ item }) => {
+    if (item) {
+        return (
+            <Card containerStyle={{ padding: 0 }}>
+                <Card.Image source={ item.image }>
+                    <View style={{ justifyContent: 'center', flex: 1 }}>
+                        <Text style={{
+                            color: 'whitesmoke',
+                            textAlign: 'center',
+                            fontSize: 17
+                        }}>{ item }</Text>
+                    </View>
+                </Card.Image>
+            </Card>
+        )
+    }
+
+    return
+}
 
 const HomeScreen = () => {
+    const [campsites, setCampsites] = useState(CAMPSITES);
+    const [partners, setPartners] = useState(PARTNERS);
+    const [promotions, setPromotions] = useState(PROMOTIONS);
+
     return (
-        <View>
+        <ScrollView>
             <Text>The Home Screen.</Text>
-        </View>
+        </ScrollView>
     )
 }
 
