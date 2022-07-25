4194230efb0a805b0401c3389dec140892044e61
DAVID HO <davidhorph@hotmail.com>
2022-07-25 02:16:37 -0500

    Layout for AboutScreen.js with Avatar and ListItem.

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { ScrollView, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Card } from 'react-native-elements';
+import { Card, ListItem, Avatar } from 'react-native-elements';
 import Mission from './MissionScreen';
 import { PARTNERS } from '../shared/partners';
 
@@ -11,6 +11,17 @@ const AboutScreen = () => {
     return (
         <ScrollView>
             <Mission />
+            {
+                partners.map(partner => (
+                    <ListItem key={partner.id}>
+                        <Avatar source={ partner.image } rounded />
+                        <ListItem.Content>
+                            <ListItem.Title></ListItem.Title>
+                            <ListItem.Subtitle></ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>
+                ))
+            }
         </ScrollView>
     )
 }
