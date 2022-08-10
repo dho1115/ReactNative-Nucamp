b85419e69b4b02c92344ecad70c7b47118f74480
DAVID HO <davidhorph@hotmail.com>
2022-08-10 18:41:58 -0500

    Added new Input fields to handeRegister inside LoginScreen.js. 4:37 of 3. Exercise: Register Form.

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -110,7 +110,98 @@ const LoginTab = ({navigation}) => {
 }
 
 const RegisterTab = () => {
-    return <ScrollView></ScrollView>
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [remember, setRemember] = useState(false);
+
+    const handleRegister = () => {
+        console.log({ username, password, firstName, lastName, email, remember });
+
+        if (remember) {
+            SecureStore.setItemAsync(
+                'userinfo', //This is the key.
+                JSON.stringify({ username, password }) //This is the value.
+            ).catch(error => console.error({ error, errorCode: error.code, errorMessage: "Cound Not Save Data: " + error.message }));
+        } else {
+            SecureStore.deleteItemAsync('userinfo')
+                .then(() => console.log("Successfully Deleted."))
+                .catch(error => console.error({ error, errorCode: error.code, errorMessage: "Cound Not Delete Data: " + error.message }))
+        }
+    }
+
+    return ( 
+        <ScrollView>
+            <View style={styles.container}>
+                <Text>Hello, {username}.</Text>
+                <Input
+                    placeholder='Username'
+                    leftIcon={{ type: 'font-awesome', name: 'user-o'}}
+                    onChangeText={(text) => setUsername(text)}
+                    value={username}
+                    containerStyle={styles.formInput}
+                    leftIconContainerStyle={styles.formIcon}
+                />
+                <Input
+                    placeholder='Password'
+                    leftIcon={{ type: 'font-awesome', name: 'key' }}
+                    onChangeText={(text) => setPassword(text)}
+                    value={password}
+                    containerStyle={styles.formInput}
+                    leftIconContainerStyle={styles.formIcon}
+                />
+                <Input
+                    placeholder='First Name'
+                    leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                    onChangeText={(text) => setFirstName(text)}
+                    value={firstName}
+                    containerStyle={styles.formInput}
+                    leftIconContainerStyle={styles.formIcon}
+                />
+                <Input
+                    placeholder='Last Name'
+                    leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                    onChangeText={(text) => setLastName(text)}
+                    value={lastName}
+                    containerStyle={styles.formInput}
+                    leftIconContainerStyle={styles.formIcon}
+                />
+                <Input
+                    placeholder='email'
+                    leftIcon={{ type: 'font-awesome', name: 'envelope-o' }}
+                    onChangeText={(text) => setEmail(text)}
+                    value={email}
+                    containerStyle={styles.formInput}
+                    leftIconContainerStyle={styles.formIcon}
+                />
+                <CheckBox
+                    title='Remember Me.'
+                    center
+                    checked={remember}
+                    onPress={() => setRemember(!remember)}
+                    containerStyle={styles.formCheckbox}
+                />
+                <View style={styles.formButton}>
+                    <Button
+                        onPress={() => handleRegister()}
+                        title='REGISTER NEW USER.'
+                        color='#5637DD'
+                        icon={
+                            <Icon
+                                name='sign-in'
+                                type='font-awesome'
+                                color='lightseagreen'
+                                iconStyle={{ marginRight: 9 }}
+                            />
+                        }
+                        buttonStyle={{ backgroundColor: 'magenta'}}
+                    />
+                </View>
+            </View>
+        </ScrollView> 
+    )
 }
 
 const Tab = createBottomTabNavigator();
